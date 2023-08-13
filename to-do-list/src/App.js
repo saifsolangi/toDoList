5bca9f96d55e302e922274e0461263c909f70397
Saif Ali <saifsolangi2@gmail.com>
2023-08-14 03:56:53 +0500

    added delete functionality to my to do list

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -5,18 +5,35 @@ function App() {
   const [newTask, setNewTask] = useState("")
 
   const handleChange = (event) => {
-    setNewTask(event.target.value)
+    if (event.target.value === 0) {
+      return
+
+    } else
+      return setNewTask(event.target.value)
   }
 
   const addTask = () => {
-    setToDoList([...toDoList, newTask])
+
+    const task = {
+      id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
+      taskName: newTask
+    }
+
+    console.log(task)
+
+    setToDoList([...toDoList, task])
+  }
+
+  const deleteTask = (id) => {
+    setToDoList(toDoList.filter((task) => task.id !== id));
+
   }
 
 
   return (
     <div className='flex flex-col items-center my-10 '>
       <div className="flex justify-center gap-2">
-        <input type="text" placeHolder="Write Task" onChange={handleChange}
+        <input type="text" placeholder="Write Task" required onChange={handleChange}
           className="border-2 border-slate-300 rounded-lg px-2" />
         <button onClick={addTask} className=" bg-blue-600 px-4 py-2 rounded-lg font-bold text-white hover:bg-blue-500">
           Add Task
@@ -25,7 +42,13 @@ function App() {
 
       <div>
         <h1 className='font-bold my-5'>{toDoList.map((task) => {
-          return <h1>{task}</h1>
+          return (
+            <div className='flex gap-5 justify-between items-center my-3'>
+              <h1>{task.taskName}</h1>
+              <button onClick={() => deleteTask(task.id)} className=' bg-red-600 px-2 py-1 rounded-md font-bold text-white hover:bg-red-500'>Delete</button>
+            </div>
+
+          );
         })}</h1>
       </div>
     </div>
